50d0128064a81bb580e65d2db897bd6f9b4700ba
QianChenShiJIN <jinshiit@yeah.net>
2016-07-09 21:13:57 +0800

    创建execise.php文件，实现合并数组。

---
+++ execise.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * execise
+ */
+
+  $arr = array('apple'=>'red','orange' =>'green',1,2);
+  $list = array('apple' =>'blue','orange'=>'yello',3,4);
+
+  $list = array_merge($list,$arr);
+
+   var_dump($list);
+
